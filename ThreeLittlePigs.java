be8485cb56dfee40c44fa73a7534624c2944e33a
bamb1sh0 <pro100ladikalexey@gmail.com>
2021-12-15 21:28:23 +0300

    Update ThreeLittlePigs.java

--- ThreeLittlePigs.java
+++ ThreeLittlePigs.java
@@ -1,47 +1,44 @@
 //Порождающий. Шаблон билдер
 
-import java.util.Locale;
-
 public class ThreeLittlePigs {
+    public static void main(String[] args) {
 
-    interface builder {
-        public void buildBase();
-
-        public void buildWaals();
-
-        public void buildRoof();
     }
 
-    class NifNif implements builder {
 
+    interface Builder {
+        public abstract void buildBase();
+
+        public abstract void buildWalls();
+
+        public abstract void buildRoof();
+    }
+
+    class NifNif implements Builder {
         @Override
         public void buildBase() {
             System.out.println("I build the soil base");
-
         }
 
         @Override
-        public void buildWaals() {
+        public void buildWalls() {
             System.out.println("I build the clay walls");
-
         }
 
         @Override
         public void buildRoof() {
             System.out.println("I build the straw roof");
-
         }
     }
 
-    class NafNaf implements builder {
-
+    class NafNaf implements Builder {
         @Override
         public void buildBase() {
             System.out.println("I build the sand base");
         }
 
         @Override
-        public void buildWaals() {
+        public void buildWalls() {
             System.out.println("I build the sticks walls");
         }
 
@@ -51,15 +48,14 @@ public class ThreeLittlePigs {
         }
     }
 
-    class NufNuf implements builder {
-
+    class NufNuf implements Builder {
         @Override
         public void buildBase() {
             System.out.println("I build the concrete base");
         }
 
         @Override
-        public void buildWaals() {
+        public void buildWalls() {
             System.out.println("I build the bricks walls");
         }
 
@@ -69,7 +65,30 @@ public class ThreeLittlePigs {
         }
     }
 
-    class Director{
+
+    class Director {
         private Builder builder;
+
+        public void setBuilder(Builder builder) {
+            this.builder = builder;
+        }
+
+        public void buildHouse() {
+            builder.buildBase();
+            builder.buildWalls();
+            builder.buildRoof();
+        }
+
+        public void buildBase(Builder builder) {
+            builder.buildBase();
+        }
+
+        public void buildWalls(Builder builder) {
+            builder.buildWalls();
+        }
+
+        public void buildRoof(Builder builder) {
+            builder.buildRoof();
+        }
     }
 }
